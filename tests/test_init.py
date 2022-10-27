18f13dfcefba70a6d253c14fac600cf4b29d64b9
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2022-10-27 14:46:51 +0200

    test __init__.py

---
+++ tests/test_init.py
@@ -0,0 +1,10 @@
+import orthomap
+
+def test_title():
+    assert orthomap.__title__ ==  "ortjomap"
+
+def test_version():
+    assert orthomap.__version__ == "0.0.1"
+
+def test_license():
+    assert orthomap.__license__ == "GPL-3"
