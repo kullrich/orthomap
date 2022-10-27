6951fd0bcc2091bea15917273b9c476624548f69
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2022-10-27 17:47:19 +0200

    test that the tax database exists

--- tests/test_ncbitax.py
+++ tests/test_ncbitax.py
@@ -1,8 +1,16 @@
 from orthomap import ncbitax
 import argparse
-from os.path import exists
+import os
 
 
 def test_define_parse():
     parse = ncbitax.define_parser()
     assert isinstance(parse, argparse.ArgumentParser)
+
+def test_update_ncbi():
+    path = os.path.expanduser('~/.etetoolkit/taxa.sqlite')
+    path_exist = os.path.exists(path)
+
+    if path_exist == False:
+        ncbitax.update_ncbi()
+        assert os.path.exists(path)
