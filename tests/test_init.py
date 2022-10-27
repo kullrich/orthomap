77771eef5816965b2455a4cf0c45c1878d79284a
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2022-10-27 16:19:00 +0200

    corrects title of the package

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,10 +1,13 @@
 import orthomap
 
+
 def test_title():
-    assert orthomap.__title__ ==  "ortjomap"
+    assert orthomap.__title__ == "orthomap"
+
 
 def test_version():
     assert orthomap.__version__ == "0.0.1"
+
 
 def test_license():
     assert orthomap.__license__ == "GPL-3"
