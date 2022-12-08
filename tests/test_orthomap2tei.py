2a35d252fd38fba060c9db43be5cd7ab82e6e3b2
Kristian Ullrich <ullrich@evolbio.mpg.de>
2022-12-08 05:34:36 +0100

    # include small example

--- tests/test_orthomap2tei.py
+++ tests/test_orthomap2tei.py
@@ -1,72 +1,63 @@
-import pandas as pd
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 
-from orthomap import orthomap2tei
+import pandas as pd
+from orthomap import orthomap2tei, datasets
 
 
 def test_read_orthomap():
-    expected_columns = ["GeneID", "Phylostratum"]
-    query_orthomap = orthomap2tei.read_orthomap("examples/Sun2021_Orthomap.tsv")
-
+    expected_columns = ['GeneID', 'Phylostratum']
+    query_orthomap = orthomap2tei.read_orthomap('examples/Sun2021_Orthomap.tsv')
     assert isinstance(query_orthomap, pd.DataFrame)
     assert all(query_orthomap.columns == expected_columns)
 
 
 def test_geneset_overlap():
-    g1 = ["A", "B", "C"]
-    g2 = ["A", "D", "E", "F"]
-
+    g1 = ['A', 'B', 'C']
+    g2 = ['A', 'D', 'E', 'F']
     overlap = orthomap2tei.geneset_overlap(g1, g2)
-
-    assert overlap["g1_g2_overlap"].values == 1
-    assert overlap["g1_ratio"].values == 1 / 3
-    assert overlap["g2_ratio"].values == 1 / 4
+    assert overlap['g1_g2_overlap'].values == 1
+    assert overlap['g1_ratio'].values == 1 / 3
+    assert overlap['g2_ratio'].values == 1 / 4
 
 
 def test_replace_by():
     # Note that with this function you always have to replace all the elements.
-    x = ["A", "B", "C"]
-    excepted_x = ["D", "K", "E"]
-    xmatch = ["A", "C", "B"]
-    xreplace = ["D", "E", "K"]
-
+    x = ['A', 'B', 'C']
+    excepted_x = ['D', 'K', 'E']
+    xmatch = ['A', 'C', 'B']
+    xreplace = ['D', 'E', 'K']
     newx = orthomap2tei.replace_by(x, xmatch, xreplace)
-
     assert newx == excepted_x
 
 
 def test_keep_min_max_case_max():
     # Not sure we need a function for this. It can be done in two lines.
     my_orthomap = pd.DataFrame.from_dict(
-        {"GeneID": ["g1", "g1", "g2", "g3", "g3"], "Phylostrata": [3, 1, 2, 5, 7]}
+        {'GeneID': ['g1', 'g1', 'g2', 'g3', 'g3'], 'Phylostrata': [3, 1, 2, 5, 7]}
     )
-
-    ascending = {"max": False, "min": True}
+    ascending = {'max': False, 'min': True}
     expected_orthomap = (
-        my_orthomap.sort_values("Phylostrata", ascending=ascending["max"])
-        .drop_duplicates("GeneID")
-        .sort_values("Phylostrata", ascending=ascending["max"])
+        my_orthomap.sort_values('Phylostrata', ascending=ascending['max'])
+        .drop_duplicates('GeneID')
+        .sort_values('Phylostrata', ascending=ascending['max'])
     )
-
-    filtered_orthomap = orthomap2tei.keep_min_max(my_orthomap, keep="max")
-
+    filtered_orthomap = orthomap2tei.keep_min_max(my_orthomap, keep='max')
     assert (expected_orthomap.values == filtered_orthomap.values).all()
 
 
 def test_keep_min_max_case_min():
     # Not sure we need a function for this. It can be done in two lines.
     my_orthomap = pd.DataFrame.from_dict(
-        {"GeneID": ["g1", "g1", "g2", "g3", "g3"], "Phylostrata": [3, 1, 2, 5, 7]}
+        {'GeneID': ['g1', 'g1', 'g2', 'g3', 'g3'], 'Phylostrata': [3, 1, 2, 5, 7]}
     )
-
-    ascending = {"max": False, "min": True}
+    ascending = {'max': False, 'min': True}
     expected_orthomap = (
-        my_orthomap.sort_values("Phylostrata", ascending=ascending["min"])
-        .drop_duplicates("GeneID")
-        .sort_values("Phylostrata", ascending=ascending["min"])
+        my_orthomap.sort_values('Phylostrata', ascending=ascending['min'])
+        .drop_duplicates('GeneID')
+        .sort_values('Phylostrata', ascending=ascending['min'])
     )
-
-    filtered_orthomap = orthomap2tei.keep_min_max(my_orthomap, keep="min")
-
+    filtered_orthomap = orthomap2tei.keep_min_max(my_orthomap, keep='min')
     assert (expected_orthomap.values == filtered_orthomap.values).all()
 
 
@@ -95,7 +86,6 @@ def test_get_pstrata():
 def test_min_max_to_01():
     ndarray_example_one = [1 for _ in range(5)]
     assert orthomap2tei.min_max_to_01(ndarray_example_one) == [0 for _ in range(5)]
-
     ndarray_example_two = [1 for _ in range(5)] + [2]
     assert orthomap2tei.min_max_to_01(ndarray_example_two) == [0 for _ in range(5)] + [1]
 
